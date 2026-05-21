d058fc987b15e94591a041dc02b6b8da266196db
srjuninn <ryanferreiradasilva2006@gmail.com>
2026-05-21 17:29:46 -0300

    feat: add user response

---
+++ src/main/java/com/project/ecommerce/responses/UserResponse.java
@@ -0,0 +1,13 @@
+package com.project.ecommerce.responses;
+
+import com.project.ecommerce.enums.RolesEnum;
+
+import java.util.UUID;
+
+public record UserResponse(
+        UUID id,
+        String name,
+        String email,
+        RolesEnum role
+) {
+}
