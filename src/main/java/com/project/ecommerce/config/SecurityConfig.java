df54feea9938a32efac7b019549b42addea75060
srjuninn <ryanferreiradasilva2006@gmail.com>
2026-05-25 17:39:31 -0300

    feat: add jwt filter config

--- src/main/java/com/project/ecommerce/config/SecurityConfig.java
+++ src/main/java/com/project/ecommerce/config/SecurityConfig.java
@@ -3,29 +3,46 @@ package com.project.ecommerce.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfig {
-//    implementação da segunraça das rotas que tiver no projeto ↓
+    private final JWTFilter jwtFilter;
+
+    public SecurityConfig(JWTFilter jwtFilter) {
+        this.jwtFilter = jwtFilter;
+    }
+
+    //    implementação da segunraça das rotas que tiver no projeto ↓
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http){
-        http
+        return http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.POST, "/users").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/users").permitAll()
+                        .requestMatchers(HttpMethod.PUT, "/users/**").hasAnyRole("USER", "ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
 //                        .requestMatchers().hasAnyRole("USER", "ADMIN")
 //                        .requestMatchers().hasRole("ADMIN")
-                        .anyRequest().authenticated()).httpBasic(withDefaults());
-        return http.build();
+                        .anyRequest().authenticated()).addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
     }
     @Bean
     PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
