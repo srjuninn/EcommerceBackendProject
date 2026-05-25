5ed760b0db330c0e5ff71d5ede49363bef30a741
srjuninn <ryanferreiradasilva2006@gmail.com>
2026-05-25 17:38:36 -0300

    feat: add update user

--- src/main/java/com/project/ecommerce/services/UserService.java
+++ src/main/java/com/project/ecommerce/services/UserService.java
@@ -40,19 +40,44 @@ public class UserService {
         String pathPhoto = photoService.savePhoto(photo);
 
 
-        UserEntity newUser = new UserEntity(userReq.name(), userReq.email(), passwordEncoder.encode(userReq.password()), RolesEnum.ROLE_USER, userReq.photo());
+        UserEntity newUser = new UserEntity(userReq.name(), userReq.email(), passwordEncoder.encode(userReq.password()), RolesEnum.ROLE_USER, pathPhoto);
 
         userRepository.save(newUser);
 
         return new UserResponse(newUser.getId(), newUser.getName(), newUser.getEmail());
     }
 
-//    Método pra ver todos os usuários
+    //    Método pra ver todos os usuários
     public List<UserResponse> showAllUsers() {
         List<UserEntity> users = userRepository.findAll();
 
         return users.stream().map(user -> new UserResponse(user.getId(), user.getName(), user.getEmail())).toList();
     }
 
+    //    Método pro proprio usuario ou admin atualizar os dados
+    public UserResponse updateUser(UUID id, UserRequest userReq, MultipartFile photo) throws IOException {
+//        pegando o email do usuário autenticado
+        String loggedEmail = SecurityContextHolder.getContext().getAuthentication().getName();
 
+        UserEntity loggedUser = userRepository.findByEmail(loggedEmail).orElseThrow(() -> new UsernameNotFoundException("Usuário autenticado não encontrado"));
+
+//        verificar se é o proprio user ou admin
+        if (!loggedUser.getId().equals(id) && !loggedUser.getRole().equals(RolesEnum.ROLE_ADMIN)) {
+            throw new AccessDeniedException("você não pode atualizar esses dados de outro usuário");
+        }
+
+        UserEntity userToUpdate = userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException(("usuário não encontrado")));
+
+        userToUpdate.setName(userReq.name());
+        userToUpdate.setPassword(passwordEncoder.encode(userReq.password()));
+
+        if (photo != null && !photo.isEmpty()) {
+            String pathPhoto = photoService.savePhoto(photo);
+            userToUpdate.setPhoto(pathPhoto);
+        }
+
+        userRepository.save(userToUpdate);
+
+        return new UserResponse(userToUpdate.getId(), userToUpdate.getName(), userToUpdate.getEmail());
+    }
 }
